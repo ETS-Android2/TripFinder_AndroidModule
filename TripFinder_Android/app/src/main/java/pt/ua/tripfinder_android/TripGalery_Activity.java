c79bc8d112c6cd0579bb108576d853f40d154654
Rodrigo Santos <56035598+RodrigoSantosgit@users.noreply.github.com>
2022-01-16 16:57:46 +0000

    Add files via upload

--- TripFinder_Android/app/src/main/java/pt/ua/tripfinder_android/TripGalery_Activity.java
+++ TripFinder_Android/app/src/main/java/pt/ua/tripfinder_android/TripGalery_Activity.java
@@ -6,34 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class TripInfo_Activity extends AppCompatActivity {
+public class TripGalery_Activity extends AppCompatActivity {
 
     private BottomNavigationView navBar;
-    private Button b_galery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trip_info);
+        setContentView(R.layout.activity_trip_galery);
 
         navBar = findViewById(R.id.navBar);
-        b_galery = findViewById(R.id.b_TripGalery);
-
         navBar.setSelectedItemId(R.id.home);
-
-        b_galery.setOnClickListener(new Button.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), TripGalery_Activity.class));
-                overridePendingTransition(0,0);
-            }
-        });
 
         navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
