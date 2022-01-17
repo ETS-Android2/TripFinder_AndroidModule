d8b37cea9d143f6f71f9eb066f11135c96ec54e9
Rodrigo Santos <56035598+RodrigoSantosgit@users.noreply.github.com>
2022-01-17 15:47:18 +0000

    mytrips page

--- TripFinder_Android/app/src/main/java/pt/ua/tripfinder_android/Mytrips_activity.java
+++ TripFinder_Android/app/src/main/java/pt/ua/tripfinder_android/Mytrips_activity.java
@@ -2,39 +2,49 @@ package pt.ua.tripfinder_android;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class profile extends AppCompatActivity {
+public class Mytrips_activity extends AppCompatActivity {
 
     private BottomNavigationView navBar;
-    private Button b_mytrips;
+    private RecyclerView mytrip_rv;
+    private String[] trips, trips_dsc;
+    private CustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_mytrips);
+
+        mytrip_rv = findViewById(R.id.mytrips_rv);
+
+        trips = new String[2];
+        trips_dsc = new String[2];
+
+
+        trips_dsc[0] = "teste 1";
+        trips_dsc[1] = "teste 2";
+
+        trips[0] = "Ria de Aveiro";
+        trips[1] = "Gastronomia";
+
+        LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        mytrip_rv.setLayoutManager(llm);
+        mytrip_rv.setItemAnimator(new DefaultItemAnimator());
+        adapter = new CustomAdapter(trips, trips_dsc);
+        mytrip_rv.setAdapter( adapter );
 
         navBar = findViewById(R.id.navBar);
-        b_mytrips = findViewById(R.id.b_tripsmade);
-
         navBar.setSelectedItemId(R.id.profile);
-
-        b_mytrips.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), Mytrips_activity.class));
-                overridePendingTransition(0,0);
-            }
-
-        });
 
         navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -42,6 +52,8 @@ public class profile extends AppCompatActivity {
 
                 switch (item.getItemId()){
                     case R.id.profile:
+                        startActivity(new Intent(getApplicationContext(), profile.class));
+                        overridePendingTransition(0,0);
                         return true;
 
                     case R.id.home:
