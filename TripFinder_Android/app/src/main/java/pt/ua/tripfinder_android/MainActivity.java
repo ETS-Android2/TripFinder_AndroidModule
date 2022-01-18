4177b902b737d13e3dd0d8f26c60e366816bfa8f
Rodrigo Santos <56035598+RodrigoSantosgit@users.noreply.github.com>
2022-01-18 18:41:36 +0000

    Add files via upload

--- TripFinder_Android/app/src/main/java/pt/ua/tripfinder_android/MainActivity.java
+++ TripFinder_Android/app/src/main/java/pt/ua/tripfinder_android/MainActivity.java
@@ -9,6 +9,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -70,6 +74,28 @@ public class MainActivity extends AppCompatActivity {
 
 
                 return false;
+            }
+        });
+
+        Spinner spinner = (Spinner) findViewById(R.id.cities_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.cities, R.layout.spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
 
